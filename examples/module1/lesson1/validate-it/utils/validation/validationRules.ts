5d190c2167d2ca0670cf2c10ee8115d7fc131bf3
RobertHaba <sedkipl98@gmail.com>
2024-02-20 23:54:42 +0100

    feat: add validation rules: required, isNumber, isLessThan, isGreaterThan, isEven

---
+++ examples/module1/lesson1/validate-it/utils/validation/validationRules.ts
@@ -0,0 +1,10 @@
+export const required = (value: any) => !!value;
+
+export const isNumber = (value: any) => typeof value === 'number';
+
+export const isLessThan = (value: number, { limit = 0 } = {}) => value < limit;
+
+export const isGreaterThan = (value: number, { limit = 0 } = {}) =>
+  value > limit;
+
+export const isEven = (value: number) => value % 2 === 0;
