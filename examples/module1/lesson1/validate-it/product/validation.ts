7a61a28b2419dbbf2de27217ac3c715a3a7ba070
RobertHaba <sedkipl98@gmail.com>
2024-02-20 23:53:42 +0100

    feat: refactor and use validation form utils

---
+++ examples/module1/lesson1/validate-it/product/validation.ts
@@ -0,0 +1,56 @@
+import { validateWithInfo } from '../utils/validation';
+import {
+  required,
+  isEven,
+  isGreaterThan,
+  isLessThan,
+  isNumber,
+} from '../utils/validation/validationRules.ts';
+
+function validator() {
+  const inputQuantity = document.getElementById(
+    'input-quantity'
+  ) as HTMLInputElement;
+  const result = document.getElementById('result') as HTMLElement;
+
+  const initButtons = () => {
+    const buttonValidate = document.getElementById('button-validate');
+    const buttonClear = document.getElementById('button-clear');
+
+    buttonValidate?.addEventListener('click', handleValidate);
+    buttonClear?.addEventListener('click', handleClearForm);
+  };
+
+  const handleValidate = () => {
+    const { message } = validateInputQuantity();
+    setValidationMessage(message);
+  };
+
+  const validateInputQuantity = () => {
+    return validateWithInfo({
+      value: +inputQuantity.value,
+      rules: [
+        { rule: isNumber },
+        { rule: required },
+        { rule: isEven },
+        { rule: isGreaterThan },
+        { rule: isLessThan, options: { limit: 100 } },
+      ],
+    });
+  };
+
+  const setValidationMessage = (message: string) => {
+    result.innerText = message;
+  };
+
+  const handleClearForm = () => {
+    clearInputValue();
+    setValidationMessage('');
+  };
+
+  const clearInputValue = () => inputQuantity.value;
+
+  initButtons();
+}
+
+export default validator;
