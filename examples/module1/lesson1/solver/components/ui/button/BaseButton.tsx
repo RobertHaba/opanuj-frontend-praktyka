effa3324c4dedb007251045dee3111471527da40
RobertHaba <sedkipl98@gmail.com>
2024-02-21 16:23:09 +0100

    feat: add new component BaseButton

---
+++ examples/module1/lesson1/solver/components/ui/button/BaseButton.tsx
@@ -0,0 +1,12 @@
+const BaseButton = ({ onClick, children }) => {
+  return (
+    <button
+      className="bg-blue-200 px-2 py-4 text-lg hover:bg-blue-500 hover:text-white rounded-md"
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default BaseButton;
