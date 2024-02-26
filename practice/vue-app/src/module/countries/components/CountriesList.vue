c8db4f692290fb65be5d20b760f6c133acae2f1a
RobertHaba <sedkipl98@gmail.com>
2024-02-26 22:27:49 +0100

    feat add countries list

---
+++ practice/vue-app/src/module/countries/components/CountriesList.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { Country } from '../types/Countries';
+import CountriesCard from './CountriesCard.vue';
+
+interface Props {
+  countries: Country[];
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+  <ul class="flex flex-col gap-4" v-if="countries.length">
+    <li v-for="country in countries" :key="country.name.common">
+      <CountriesCard
+        :name="country.name.common"
+        :flag="country.flags"
+        :population="country.population"
+      />
+    </li>
+  </ul>
+</template>
