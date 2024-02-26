3e5da54c39eb1e124a553d9713d3b08ff726248b
RobertHaba <sedkipl98@gmail.com>
2024-02-26 22:28:02 +0100

    feat: add countries select

---
+++ practice/vue-app/src/module/countries/components/form/CountriesSortSelect.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import MSelect from '@/components/ui/form/MSelect.vue';
+import { SortOption } from '../../types/Countries';
+</script>
+
+<template>
+  <MSelect
+    v-bind="$attrs"
+    :items="[
+      { value: SortOption.NameAsc, label: 'Name: A-Z' },
+      { value: SortOption.NameDesc, label: 'Name: Z-A' },
+      { value: SortOption.PopulationAsc, label: 'Population: Low-High' },
+      { value: SortOption.PopulationDesc, label: 'Population: High-low' },
+    ]"
+  />
+</template>
