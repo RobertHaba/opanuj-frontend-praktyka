b4c59e6568095c506c58b6894e33ef0f7b4b5173
RobertHaba <sedkipl98@gmail.com>
2024-02-26 22:27:22 +0100

    feat: add country card

---
+++ practice/vue-app/src/module/countries/components/CountriesCard.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+interface Props {
+  name: string;
+  flag: { alt: string; svg: string };
+  population: number;
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+  <div
+    class="flex justify-between p-4 items-center bg-zinc-800 border border-zinc-600 rounded-lg"
+  >
+    <div class="flex gap-4 items-center">
+      <span
+        class="w-6 h-6 bg-no-repeat bg-center bg-contain"
+        :style="{ 'background-image': `url(${flag.svg})` }"
+      />
+      <h2 class="text-lg font-semibold">{{ name }}</h2>
+    </div>
+
+    <span class="text-sm text-zinc-400">{{ population }}</span>
+  </div>
+</template>
