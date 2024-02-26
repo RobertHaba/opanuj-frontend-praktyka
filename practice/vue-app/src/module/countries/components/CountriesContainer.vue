f88f234652c6ad03de98461807b78f2a343e554f
RobertHaba <sedkipl98@gmail.com>
2024-02-26 23:06:01 +0100

    feat: moved sort label into Countries sort select, add name into search

--- practice/vue-app/src/module/countries/components/CountriesContainer.vue
+++ practice/vue-app/src/module/countries/components/CountriesContainer.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
+import useSort from '../composables/useSort';
+import useSearch from '../composables/useSearch';
+
 import CountriesFilterSelect from './form/CountriesFilterSelect.vue';
 import AInput from '@/components/ui/form/AInput.vue';
 import AButton from '@/components/ui/button/AButton.vue';
 import CountriesList from './CountriesList.vue';
 import CountriesSortSelect from './form/CountriesSortSelect.vue';
 import AHeaderForm from '@/components/ui/header/AHeaderForm.vue';
-import useSort from '../composables/useSort';
-import useSearch from '../composables/useSearch';
 
 const { countries, filter, searchCountries } = useSearch();
 
@@ -14,19 +15,20 @@ const { sortOption, sortedCountries } = useSort(countries);
 </script>
 
 <template>
-  <div class="flex flex-col gap-8 p-4">
+  <div class="flex relative flex-col gap-8 p-4">
     <AHeaderForm>
-      <form class="flex gap-2" @submit.prevent="searchCountries">
+      <form class="flex gap-2" role="search" @submit.prevent="searchCountries">
         <CountriesFilterSelect v-model="filter.type" />
-        <AInput class="w-full" v-model="filter.value" />
+        <AInput
+          type="search"
+          :name="`country-${filter.type}`"
+          class="w-full"
+          v-model="filter.value"
+        />
         <AButton @click="searchCountries" label="Search" />
       </form>
 
-      <div class="flex gap-2 items-center">
-        <span>Sort</span>
-
-        <CountriesSortSelect v-model="sortOption" />
-      </div>
+      <CountriesSortSelect v-model="sortOption" />
     </AHeaderForm>
 
     <CountriesList :countries="sortedCountries" />
