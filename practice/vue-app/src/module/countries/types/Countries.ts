6286696c5f7a54013ce6b3af0c8a84bde522d41d
RobertHaba <sedkipl98@gmail.com>
2024-02-26 22:28:20 +0100

    feat: add types for country

---
+++ practice/vue-app/src/module/countries/types/Countries.ts
@@ -0,0 +1,21 @@
+export enum FilterType {
+  All = 'all',
+  Name = 'name',
+  Currency = 'currency',
+  Language = 'language',
+  Capital = 'capital',
+}
+
+export enum SortOption {
+  NameAsc = 'name-asc',
+  NameDesc = 'name-desc',
+  PopulationAsc = 'population-asc',
+  PopulationDesc = 'population-desc',
+}
+
+// part of Country
+export interface Country {
+  name: { common: string };
+  flags: { alt: string; svg: string };
+  population: number;
+}
