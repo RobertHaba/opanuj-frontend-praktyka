39429a5283f02c4f39dcf84a8256dbf79984dc4c
RobertHaba <sedkipl98@gmail.com>
2024-02-26 22:28:11 +0100

    feat: add countries service

---
+++ practice/vue-app/src/module/countries/services/CountriesService.ts
@@ -0,0 +1,22 @@
+import { FilterType } from '../types/Countries';
+
+export interface Filter {
+  value: string;
+  type: FilterType;
+}
+
+class CountriesService {
+  static URL = 'https://restcountries.com/v3.1';
+
+  async fetch({ filter: { value, type } }: { filter: Filter }) {
+    const pathToFilter = [type, value].join('/');
+
+    const data = await fetch(`${CountriesService.URL}/${pathToFilter}`)
+      .then((Response) => Response.json())
+      .then((data) => data);
+
+    return data;
+  }
+}
+
+export default new CountriesService();
