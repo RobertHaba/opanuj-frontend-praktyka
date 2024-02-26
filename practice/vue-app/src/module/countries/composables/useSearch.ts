64c9c344fd8db5058f03c8358e72e7082cd44909
RobertHaba <sedkipl98@gmail.com>
2024-02-26 23:07:08 +0100

    feat: watch for try set filter type as name and restore filter type into all when value is empty

--- practice/vue-app/src/module/countries/composables/useSearch.ts
+++ practice/vue-app/src/module/countries/composables/useSearch.ts
@@ -1,16 +1,33 @@
-import { reactive, ref } from 'vue';
+import { reactive, ref, watch } from 'vue';
 import { Country, FilterType } from '../types/Countries';
 import CountriesService, { Filter } from '../services/CountriesService';
 
 export default function useSearch() {
   const countries = ref<Country[]>([]);
+
   const filter = reactive<Filter>({ value: '', type: FilterType.All });
 
   const searchCountries = async () => {
-    countries.value = await CountriesService.fetch({
+    if (!filter.value) filter.type = FilterType.All;
+
+    const { data } = await CountriesService.fetch({
       filter,
     });
+
+    countries.value = data;
   };
+
+  const trySetFilterTypeAsName = () => {
+    if (!filter.value || filter.type !== FilterType.All) return;
+    filter.type = FilterType.Name;
+  };
+
+  watch(
+    () => filter.value,
+    () => {
+      trySetFilterTypeAsName();
+    }
+  );
 
   return { countries, searchCountries, filter };
 }
