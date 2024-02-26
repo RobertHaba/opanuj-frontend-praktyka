14ec55ebb60c9516b08657cf14bb883832995bb3
RobertHaba <sedkipl98@gmail.com>
2024-02-26 23:03:28 +0100

    feat: change to country prop, add capital info

--- practice/vue-app/src/module/countries/components/CountriesCard.vue
+++ practice/vue-app/src/module/countries/components/CountriesCard.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
+import { Country } from '../types/Countries';
+
 interface Props {
-  name: string;
-  flag: { alt: string; svg: string };
-  population: number;
+  country: Country;
 }
 
 defineProps<Props>();
@@ -12,14 +12,21 @@ defineProps<Props>();
   <div
     class="flex justify-between p-4 items-center bg-zinc-800 border border-zinc-600 rounded-lg"
   >
-    <div class="flex gap-4 items-center">
-      <span
-        class="w-6 h-6 bg-no-repeat bg-center bg-contain"
-        :style="{ 'background-image': `url(${flag.svg})` }"
-      />
-      <h2 class="text-lg font-semibold">{{ name }}</h2>
+    <div class="flex flex-col">
+      <div class="flex gap-4 items-center">
+        <span
+          class="w-6 h-6 bg-no-repeat bg-center bg-contain"
+          :style="{ 'background-image': `url(${country.flags.svg})` }"
+        />
+
+        <h2 class="text-lg font-semibold">{{ country.name.common }}</h2>
+      </div>
+
+      <span v-if="country.capital" class="text-sm text-zinc-400">
+        {{ country.capital?.[0] }}
+      </span>
     </div>
 
-    <span class="text-sm text-zinc-400">{{ population }}</span>
+    <span class="text-sm text-zinc-400">{{ country.population }}</span>
   </div>
 </template>
