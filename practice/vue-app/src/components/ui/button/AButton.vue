90e3c99b009876fc1e9680bf5f9cd56e3380396b
RobertHaba <sedkipl98@gmail.com>
2024-02-26 22:27:16 +0100

    feat: add UI components

---
+++ practice/vue-app/src/components/ui/button/AButton.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+interface Props {
+  type?: 'button' | 'submit' | 'reset';
+  label?: string;
+}
+
+withDefaults(defineProps<Props>(), { type: 'button' });
+</script>
+
+<template>
+  <button class="h-10 bg-blue-700 px-4 rounded-lg text-white" :type="type">
+    {{ label }}
+  </button>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ practice/vue-app/src/components/ui/header/AHeaderForm.vue
@@ -0,0 +1,7 @@
+<template>
+  <div
+    class="flex w-full flex-wrap gap-4 justify-between py-4 border-b border-zinc-600"
+  >
+    <slot />
+  </div>
+</template>
